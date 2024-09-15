0e331ee13a10730a5ebca0fb6fdaba66c6018e28
akash <akashmanjhi1111198@gmail.com>
2024-09-15 13:45:00 +0530

    push images

--- src/app/lenis/page.tsx
+++ src/app/lenis/page.tsx
@@ -70,26 +70,26 @@ const page = () => {
 
 <div className="w-full bg-zinc-900 relative">
 <div className="grid grid-cols-8 grid-rows-20 gap-2 overflow-hidden">
-   <div className="elem col-span-1 row-span-1" style={{ "--r": 3, "--c": 5 } as React.CSSProperties}><img src="img/1.jpg" alt="image1" /></div>
-   <div className="elem col-span-1 row-span-1" style={{ "--r": 7, "--c": 2 } as React.CSSProperties}><img src="img/2.jpg" alt="image2" /></div>
-   <div className="elem col-span-1 row-span-1" style={{ "--r": 1, "--c": 8 } as React.CSSProperties}><img src="img/3.jpg" alt="image 3" /></div>
-   <div className="elem col-span-1 row-span-1" style={{ "--r": 9, "--c": 3 } as React.CSSProperties}><img src="img/3.jpg" alt="image 4" /></div>
-   <div className="elem col-span-1 row-span-1" style={{ "--r": 5, "--c": 6 } as React.CSSProperties}><img src="img/3.jpg" alt="image 5" /></div>
-   <div className="elem col-span-1 row-span-1" style={{ "--r": 2, "--c": 4 } as React.CSSProperties}><img src="img/3.jpg" alt="image 6" /></div>
-   <div className="elem col-span-1 row-span-1" style={{ "--r": 8, "--c": 6 } as React.CSSProperties}><img src="img/3.jpg" alt="image 7" /></div>
-   <div className="elem col-span-1 row-span-1" style={{ "--r": 4, "--c": 7 } as React.CSSProperties}><img src="img/3.jpg" alt="image 8" /></div>
-   <div className="elem col-span-1 row-span-1" style={{ "--r": 6, "--c": 5 } as React.CSSProperties}><img src="img/3.jpg" alt="image 9" /></div>
-   <div className="elem col-span-1 row-span-1" style={{ "--r": 10, "--c": 2 } as React.CSSProperties }><img src="img/3.jpg" alt="image 10" /></div>
-   <div className="elem col-span-1 row-span-1" style={{ "--r": 3, "--c": 8 } as React.CSSProperties }><img src="img/3.jpg" alt="image 11" /></div>
-   <div className="elem col-span-1 row-span-1" style={{ "--r": 7, "--c": 4 } as React.CSSProperties }><img src="img/3.jpg" alt="image 12" /></div>
-   <div className="elem col-span-1 row-span-1" style={{ "--r": 1, "--c": 1 } as React.CSSProperties }><img src="img/3.jpg" alt="image 13" /></div>
-   <div className="elem col-span-1 row-span-1" style={{ "--r": 9, "--c": 1 } as React.CSSProperties }><img src="img/3.jpg" alt="image 14" /></div>
-   <div className="elem col-span-1 row-span-1" style={{ "--r": 5, "--c": 3 } as React.CSSProperties }><img src="img/3.jpg" alt="image 15" /></div>
-   <div className="elem col-span-1 row-span-1" style={{ "--r": 2, "--c": 7 } as React.CSSProperties }><img src="img/3.jpg" alt="image 16" /></div>
-   <div className="elem col-span-1 row-span-1" style={{ "--r": 8, "--c": 5 } as React.CSSProperties }><img src="img/3.jpg" alt="image 17" /></div>
-   <div className="elem col-span-1 row-span-1" style={{ "--r": 4, "--c": 2 } as React.CSSProperties }><img src="img/3.jpg" alt="image 18" /></div>
-   <div className="elem col-span-1 row-span-1" style={{ "--r": 6, "--c": 8 } as React.CSSProperties}><img src="img/3.jpg" alt="image 19" /></div>
-   <div className="elem col-span-1 row-span-1" style={{ "--r": 10, "--c": 6 } as React.CSSProperties}><img src="img/3.jpg" alt="image 20" /></div>
+   <div className="elem col-span-1 row-span-1" style={{ "--r": 3, "--c": 5 } as React.CSSProperties}><img src="img/1.jpg" alt="image1"  /></div>
+   <div className="elem col-span-1 row-span-1" style={{ "--r": 7, "--c": 3 } as React.CSSProperties}><img src="img/2.jpg" alt="image2" /></div>
+   <div className="elem col-span-1 row-span-1" style={{ "--r": 1, "--c": 7 } as React.CSSProperties}><img src="img/3.jpg" alt="image 3" /></div>
+   <div className="elem col-span-1 row-span-1" style={{ "--r": 9, "--c": 3 } as React.CSSProperties}><img src="img/4.jpg" alt="image 4" /></div>
+   <div className="elem col-span-1 row-span-1" style={{ "--r": 5, "--c": 6 } as React.CSSProperties}><img src="img/5.jpg" alt="image 5" /></div>
+   <div className="elem col-span-1 row-span-1" style={{ "--r": 2, "--c": 4 } as React.CSSProperties}><img src="img/6.jpg" alt="image 6" /></div>
+   <div className="elem col-span-1 row-span-1" style={{ "--r": 8, "--c": 6 } as React.CSSProperties}><img src="img/7.jpg" alt="image 7" /></div>
+   <div className="elem col-span-1 row-span-1" style={{ "--r": 4, "--c": 7 } as React.CSSProperties}><img src="img/8.jpg" alt="image 8" /></div>
+   <div className="elem col-span-1 row-span-1" style={{ "--r": 6, "--c": 5 } as React.CSSProperties}><img src="img/9.jpg" alt="image 9" /></div>
+   <div className="elem col-span-1 row-span-1" style={{ "--r": 10, "--c": 4 } as React.CSSProperties }><img src="img/10.jpg" alt="image 10" /></div>
+   <div className="elem col-span-1 row-span-1" style={{ "--r": 3, "--c": 8 } as React.CSSProperties }><img src="img/11.jpg" alt="image 11" /></div>
+   <div className="elem col-span-1 row-span-1" style={{ "--r": 7, "--c": 6 } as React.CSSProperties }><img src="img/12.jpg" alt="image 12" /></div>
+   <div className="elem col-span-1 row-span-1" style={{ "--r": 1, "--c": 1 } as React.CSSProperties }><img src="img/13.jpg" alt="image 13" /></div>
+   <div className="elem col-span-1 row-span-1" style={{ "--r": 9, "--c": 6 } as React.CSSProperties }><img src="img/14.jpg" alt="image 14" /></div>
+   <div className="elem col-span-1 row-span-1" style={{ "--r": 5, "--c": 3 } as React.CSSProperties }><img src="img/15.jpg" alt="image 15" /></div>
+   <div className="elem col-span-1 row-span-1" style={{ "--r": 2, "--c": 7 } as React.CSSProperties }><img src="img/16.jpg" alt="image 16" /></div>
+   <div className="elem col-span-1 row-span-1" style={{ "--r": 8, "--c": 5 } as React.CSSProperties }><img src="img/17.jpg" alt="image 17" /></div>
+   <div className="elem col-span-1 row-span-1" style={{ "--r": 4, "--c": 3 } as React.CSSProperties }><img src="img/18.jpg" alt="image 18" /></div>
+   <div className="elem col-span-1 row-span-1" style={{ "--r": 6, "--c": 8 } as React.CSSProperties}><img src="img/19.jpg" alt="image 19" /></div>
+   <div className="elem col-span-1 row-span-1" style={{ "--r": 10, "--c": 6 } as React.CSSProperties}><img src="img/20.jpg" alt="image 20" /></div>
 </div>
 
 <div className="header fixed inset-0 flex flex-col items-center justify-center  text-white">
